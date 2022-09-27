b860c319bf5e2088106f9be8615cc0a3fe26c6dd
Toxic Symphony <brayan_stiv96@hotmail.com>
2022-09-27 17:35:32 -0500

    se crea app para practicar react

---
+++ src/Componente/Brayan.js
@@ -0,0 +1,10 @@
+export function Brayan(params) {
+    let nombre = 'Brayan'
+    return (
+        <>
+            <h1>Hola react soy {nombre}</h1>
+            <hr></hr>
+        </>
+        
+    )
+}
---
+++ src/Footer/Footer.js
@@ -0,0 +1,41 @@
+export function Footer() {
+  return (
+    <>
+      <footer>
+        <div class="container mt-5 bg-dark">
+          <div class="row">
+            <div class="col-12 mt-5 d-flex justify-content-center">
+              <a
+                target="_blank"
+                href="https://www.facebook.com/"
+                style="color: rgb(231, 39, 39)"
+              >
+                <i class="bi bi-facebook fs-2 m-2"></i>
+              </a>
+              <a
+                target="_blank"
+                href="https://twitter.com/"
+                style="color: rgb(231, 39, 39)"
+              >
+                <i class="bi bi-twitter fs-2 m-2"></i>
+              </a>
+              <a
+                target="_blank"
+                href="https://www.instagram.com/"
+                style="color: rgb(231, 39, 39)"
+              >
+                <i class="bi bi-instagram fs-2 m-2"></i>
+              </a>
+            </div>
+            <div class="col-12 text-center text-light">
+              <p>videogamescult.com</p>
+            </div>
+            <div class="col-12 text-center mt-3 text-light">
+              <p>&copy; Created by Brayan Ospina & Juan Pablo Mendoza</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
